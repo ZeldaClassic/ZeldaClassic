dc66a920308800e249b31ca6c0e1fcc5f2b00395
Reiquel Applegate <reiquel.applegate@gmail.com>
2024-04-01 22:47:22 -0400

    refactor(zq): indentation

--- src/qst.cpp
+++ src/qst.cpp
@@ -19197,52 +19197,52 @@ int32_t readcheatcodes(PACKFILE *f, zquestheader *Header)
 {
 	bool should_skip = legacy_skip_flags && get_bit(legacy_skip_flags, skip_cheats);
 
-    int32_t dummy;
-    ZCHEATS tempzcheats;
-    char temp_use_cheats=1;
-    memset(&tempzcheats, 0, sizeof(tempzcheats));
-    word s_version = 0;
+	int32_t dummy;
+	ZCHEATS tempzcheats;
+	char temp_use_cheats=1;
+	memset(&tempzcheats, 0, sizeof(tempzcheats));
+	word s_version = 0;
     
-    if(Header->zelda_version > 0x192)
-    {
-        //section version info
-        if(!p_igetw(&s_version,f))
-        {
-            return qe_invalid;
-        }
-        
-	FFCore.quest_format[vCheats] = s_version;
-        //al_trace("Cheats version %d\n", dummy);
-        if(!p_igetw(&dummy,f))
-        {
-            return qe_invalid;
-        }
-        
-        //section size
-        if(!p_igetl(&dummy,f))
-        {
-            return qe_invalid;
-        }
-        
-        //finally...  section data
-        if(!p_getc(&temp_use_cheats,f))
-        {
-            return qe_invalid;
-        }
-    }
+	if(Header->zelda_version > 0x192)
+	{
+		//section version info
+		if(!p_igetw(&s_version,f))
+		{
+			return qe_invalid;
+		}
+		
+		FFCore.quest_format[vCheats] = s_version;
+		//al_trace("Cheats version %d\n", dummy);
+		if(!p_igetw(&dummy,f))
+		{
+			return qe_invalid;
+		}
+		
+		//section size
+		if(!p_igetl(&dummy,f))
+		{
+			return qe_invalid;
+		}
+		
+		//finally...  section data
+		if(!p_getc(&temp_use_cheats,f))
+		{
+			return qe_invalid;
+		}
+	}
     
-    if(Header->data_flags[ZQ_CHEATS2])
-    {
-        if(!p_igetl(&tempzcheats.flags,f))
-        {
-            return qe_invalid;
-        }
-        
-        if(!pfread(&tempzcheats.codes, sizeof(tempzcheats.codes),f))
-        {
-            return qe_invalid;
-        }
-    }
+	if(Header->data_flags[ZQ_CHEATS2])
+	{
+		if(!p_igetl(&tempzcheats.flags,f))
+		{
+			return qe_invalid;
+		}
+
+		if(!pfread(&tempzcheats.codes, sizeof(tempzcheats.codes),f))
+		{
+			return qe_invalid;
+		}
+	}
     
 	if (should_skip)
 		return 0;
@@ -19250,7 +19250,7 @@ int32_t readcheatcodes(PACKFILE *f, zquestheader *Header)
 	memcpy(&zcheats, &tempzcheats, sizeof(tempzcheats));
 	Header->data_flags[ZQ_CHEATS2]=temp_use_cheats;
     
-    return 0;
+	return 0;
 }
 
 int32_t readinitdata_old(PACKFILE *f, zquestheader *Header, word s_version, word s_cversion, zinitdata& temp_zinit)
