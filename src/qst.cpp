b1e2623feef31737f0e83df7ceedc9d4c93bdce7
Reiquel Applegate <reiquel.applegate@gmail.com>
2024-04-01 22:53:59 -0400

    refactor(zq): indentation

--- src/qst.cpp
+++ src/qst.cpp
@@ -20897,11 +20897,11 @@ int32_t readitemdropsets(PACKFILE *f, int32_t version, word build)
 {
 	bool should_skip = legacy_skip_flags && get_bit(legacy_skip_flags, skip_itemdropsets);
 
-    build=build; // here to prevent compiler warnings
-    dword dummy_dword;
-    word item_drop_sets_to_read=0;
-    item_drop_object tempitemdrop;
-    word s_version=0, s_cversion=0;
+	build=build; // here to prevent compiler warnings
+	dword dummy_dword;
+	word item_drop_sets_to_read=0;
+	item_drop_object tempitemdrop;
+	word s_version=0, s_cversion=0;
     
 	if (!should_skip)
 	for(int32_t i=0; i<MAXITEMDROPSETS; i++)
@@ -20909,108 +20909,108 @@ int32_t readitemdropsets(PACKFILE *f, int32_t version, word build)
 		memset(&item_drop_sets[i], 0, sizeof(item_drop_object));
 	}
     
-    if(version > 0x192)
-    {
-        item_drop_sets_to_read=0;
-        
-        //section version info
-        if(!p_igetw(&s_version,f))
-        {
-            return qe_invalid;
-        }
-	
-	FFCore.quest_format[vItemDropsets] = s_version;
-        
-        //al_trace("Item drop sets version %d\n", s_version);
-        if(!p_igetw(&s_cversion,f))
-        {
-            return qe_invalid;
-        }
-        
-        //section size
-        if(!p_igetl(&dummy_dword,f))
-        {
-            return qe_invalid;
-        }
-        
-        //finally...  section data
-        if(!p_igetw(&item_drop_sets_to_read,f))
-        {
-            return qe_invalid;
-        }
+	if(version > 0x192)
+	{
+		item_drop_sets_to_read=0;
+		
+		//section version info
+		if(!p_igetw(&s_version,f))
+		{
+		    return qe_invalid;
+		}
+		
+		FFCore.quest_format[vItemDropsets] = s_version;
+		
+		//al_trace("Item drop sets version %d\n", s_version);
+		if(!p_igetw(&s_cversion,f))
+		{
+		    return qe_invalid;
+		}
+		
+		//section size
+		if(!p_igetl(&dummy_dword,f))
+		{
+		    return qe_invalid;
+		}
+		
+		//finally...  section data
+		if(!p_igetw(&item_drop_sets_to_read,f))
+		{
+		    return qe_invalid;
+		}
 
-        if (!(item_drop_sets_to_read >= 0 && item_drop_sets_to_read <= MAXITEMDROPSETS))
-        {
-            return qe_invalid;
-        }
-    }
-    else
-    {
+		if (!(item_drop_sets_to_read >= 0 && item_drop_sets_to_read <= MAXITEMDROPSETS))
+		{
+		    return qe_invalid;
+		}
+	}
+	else
+	{
 		init_item_drop_sets();
-    }
+	}
     
-    if(s_version>=1)
-    {
-        for(int32_t i=0; i<item_drop_sets_to_read; i++)
-        {
-            if(!p_getstr(tempitemdrop.name,sizeof(tempitemdrop.name)-1,f))
-            {
-                return qe_invalid;
-            }
-            
-            for(int32_t j=0; j<10; ++j)
-            {
-                if(!p_igetw(&tempitemdrop.item[j],f))
-                {
-                    return qe_invalid;
-                }
-            }
-            
-            for(int32_t j=0; j<11; ++j)
-            {
-                if(!p_igetw(&tempitemdrop.chance[j],f))
-                {
-                    return qe_invalid;
-                }
-            }
-            
-            // Dec 2008: Addition of the 'Tall Grass' set, #12,
-            // overrides the quest's set #12.
-            if(s_version<2 && i==12)
-                continue;
-                
-            // Deprecated: qr_NOCLOCKS and qr_ALLOW10RUPEEDROPS
-            if(s_version<2) for(int32_t j=0; j<10; ++j)
-                {
-                    int32_t it = tempitemdrop.item[j];
-                    
-                    if((itemsbuf[it].family == itype_rupee
-                            && ((itemsbuf[it].amount)&0xFFF) == 10)
-                            && !get_bit(deprecated_rules, qr_ALLOW10RUPEEDROPS_DEP))
-                    {
-                        tempitemdrop.chance[j+1]=0;
-                    }
-                    else if(itemsbuf[it].family == itype_clock && get_bit(deprecated_rules, qr_NOCLOCKS_DEP))
-                    {
-                        tempitemdrop.chance[j+1]=0;
-                    }
-                    
-                    // From Sept 2007 to Dec 2008, non-gameplay items were prohibited.
-                    if(itemsbuf[it].family == itype_misc)
-                    {
-                        // If a non-gameplay item was selected, then item drop was aborted.
-                        // Reflect this by increasing the 'Nothing' chance accordingly.
-                        tempitemdrop.chance[0]+=tempitemdrop.chance[j+1];
-                        tempitemdrop.chance[j+1]=0;
-                    }
-                }
-            
+	if(s_version>=1)
+	{
+		for(int32_t i=0; i<item_drop_sets_to_read; i++)
+		{
+			if(!p_getstr(tempitemdrop.name,sizeof(tempitemdrop.name)-1,f))
+			{
+				return qe_invalid;
+			}
+
+			for(int32_t j=0; j<10; ++j)
+			{
+				if(!p_igetw(&tempitemdrop.item[j],f))
+				{
+					return qe_invalid;
+				}
+			}
+
+			for(int32_t j=0; j<11; ++j)
+			{
+				if(!p_igetw(&tempitemdrop.chance[j],f))
+				{
+					return qe_invalid;
+				}
+			}
+
+			// Dec 2008: Addition of the 'Tall Grass' set, #12,
+			// overrides the quest's set #12.
+			if(s_version<2 && i==12)
+			continue;
+
+			// Deprecated: qr_NOCLOCKS and qr_ALLOW10RUPEEDROPS
+			if(s_version<2) for(int32_t j=0; j<10; ++j)
+			{
+				int32_t it = tempitemdrop.item[j];
+
+				if((itemsbuf[it].family == itype_rupee
+				&& ((itemsbuf[it].amount)&0xFFF) == 10)
+				&& !get_bit(deprecated_rules, qr_ALLOW10RUPEEDROPS_DEP))
+				{
+					tempitemdrop.chance[j+1]=0;
+				}
+				else if(itemsbuf[it].family == itype_clock && get_bit(deprecated_rules, qr_NOCLOCKS_DEP))
+				{
+					tempitemdrop.chance[j+1]=0;
+				}
+
+				// From Sept 2007 to Dec 2008, non-gameplay items were prohibited.
+				if(itemsbuf[it].family == itype_misc)
+				{
+					// If a non-gameplay item was selected, then item drop was aborted.
+					// Reflect this by increasing the 'Nothing' chance accordingly.
+					tempitemdrop.chance[0]+=tempitemdrop.chance[j+1];
+					tempitemdrop.chance[j+1]=0;
+				}
+			}
+
 			if (!should_skip)
 				memcpy(&item_drop_sets[i], &tempitemdrop, sizeof(item_drop_object));
-        }
-    }
+		}
+	}
     
-    return 0;
+	return 0;
 }
 
 int32_t readfavorites(PACKFILE *f, int32_t, word)
