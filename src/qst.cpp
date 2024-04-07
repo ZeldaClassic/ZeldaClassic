bed895bc4b30ed8bb5f65ae35348fcedf1d93c45
Reiquel Applegate <reiquel.applegate@gmail.com>
2024-04-07 01:19:24 -0400

    refactor(zq): grouping deletes

--- src/qst.cpp
+++ src/qst.cpp
@@ -11635,20 +11635,16 @@ int32_t readffscript(PACKFILE *f, zquestheader *Header)
 				}
 			}
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONLAUNCH];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONLAUNCH]; 
 			globalscripts[GLOBAL_SCRIPT_ONLAUNCH] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONLAUNCH);
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONCONTGAME];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONCONTGAME]; 
 			globalscripts[GLOBAL_SCRIPT_ONCONTGAME] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONCONTGAME);
 			
-			delete globalscripts[GLOBAL_SCRIPT_F6];
-				
+			delete globalscripts[GLOBAL_SCRIPT_F6]; 
 			globalscripts[GLOBAL_SCRIPT_F6] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_F6);
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONSAVE];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONSAVE]; 
 			globalscripts[GLOBAL_SCRIPT_ONSAVE] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONSAVE);
 		}
 		else
@@ -11663,24 +11659,19 @@ int32_t readffscript(PACKFILE *f, zquestheader *Header)
 				}
 			}
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONSAVELOAD];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONSAVELOAD]; 
 			globalscripts[GLOBAL_SCRIPT_ONSAVELOAD] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONSAVELOAD);
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONLAUNCH];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONLAUNCH]; 
 			globalscripts[GLOBAL_SCRIPT_ONLAUNCH] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONLAUNCH);
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONCONTGAME];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONCONTGAME]; 
 			globalscripts[GLOBAL_SCRIPT_ONCONTGAME] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONCONTGAME);
 			
-			delete globalscripts[GLOBAL_SCRIPT_F6];
-				
+			delete globalscripts[GLOBAL_SCRIPT_F6]; 
 			globalscripts[GLOBAL_SCRIPT_F6] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_F6);
 			
-			delete globalscripts[GLOBAL_SCRIPT_ONSAVE];
-				
+			delete globalscripts[GLOBAL_SCRIPT_ONSAVE]; 
 			globalscripts[GLOBAL_SCRIPT_ONSAVE] = new script_data(ScriptType::Global, GLOBAL_SCRIPT_ONSAVE);
 		}
 		
@@ -11707,12 +11698,10 @@ int32_t readffscript(PACKFILE *f, zquestheader *Header)
 					return qe_invalid;
 				}
 			}
-			delete playerscripts[3];
-					
+			delete playerscripts[3]; 
 			playerscripts[3] = new script_data(ScriptType::Player, 3);
 			
-			delete playerscripts[4];
-					
+			delete playerscripts[4]; 
 			playerscripts[4] = new script_data(ScriptType::Player, 4);
 		}
 		if(s_version > 8 && s_version < 10)
