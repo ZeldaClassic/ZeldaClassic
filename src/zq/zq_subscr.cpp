99f1c171561d072f2f5b2ade7fa97fb6c167bcb5
Reiquel Applegate <reiquel.applegate@gmail.com>
2024-04-17 18:14:23 -0400

    refactor(zq): compressed definition of dist_obj

--- src/zq/zq_subscr.cpp
+++ src/zq/zq_subscr.cpp
@@ -1560,14 +1560,7 @@ void grid_snap_objects(bool *selection, int32_t snap_type)
 struct dist_obj
 {
 	int32_t index;
-	int32_t l;
-	int32_t t;
-	int32_t w;
-	int32_t h;
-	int32_t r;
-	int32_t b;
-	int32_t c;
-	int32_t m;
+	int32_t l, t, w, h, r, b, c, m;
 };
 
 void distribute_objects(bool *, int32_t distribute_type)
